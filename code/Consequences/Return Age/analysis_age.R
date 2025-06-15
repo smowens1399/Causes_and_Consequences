c29d584a207eedb895a4e726507d27e63c714910
smowens1399 <sowens1399@gmail.com>
2025-06-15 13:50:31 -0500

    Delete code directory

--- code/Consequences/Return Age/analysis_age.R
+++
@@ -1,7 +0,0 @@
-################### Analysis for age at ocean return ######################
-library(tidyverse)
-### Read in Data
-age<-read.csv("processed_data/consequences/age.csv")
-
-### Perform t-test
-t.test(age~strat,data=age)
